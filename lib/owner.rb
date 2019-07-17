6665c931242b69717ad73d23793d1e7db67108e2
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-17 21:52:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Owner
-  attr_reader :name, :species
-  attr_accessor :cats, :dogs, :all_pets
+  attr_reader :name, :species, :all_pets
+  attr_accessor :cats, :dogs
   
   @@all = []
   @@count = 0
