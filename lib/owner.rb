289def07213926e2c8da3986cb86b0fc340cdeb4
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-17 21:48:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,9 +57,9 @@ class Owner
       binding.pry
       pet.mood = "nervous"
       
-      # pet.owner = nil
+      pet.owner = nil
     end
-    # @cats.clear
-    # @dogs.clear
+    @cats.clear
+    @dogs.clear
   end
 end
